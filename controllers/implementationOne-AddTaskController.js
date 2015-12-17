89a467a8b90a5d0bc669b9198c99afb798779df7
julatey <mkpannah@gmail.com>
2015-12-17 22:41:20 +0000

    Add Task Controller Java script
    It helps create an interactive view for the add task .

---
+++ controllers/implementationOne-AddTaskController.js
@@ -0,0 +1,17 @@
+//function to call the add task
+            $ ( function ( )
+            {
+                $ ( ".newtaskbutton" ).click ( function ( )
+                {
+                    $(".add").slideDown ( 'slow', function ( )
+                    {
+                        $(".addtaskbutton").slideDown().show();
+                        $(".newtaskbutton").slideDown().hide();
+                         $(".updatetaskbutton").slideDown().show();
+                         $(".edittaskbutton").slideDown().hide();
+                        $(this).show();
+                    });
+                    $(".preview").hide();
+                    $(".update").hide();
+                });
+            });

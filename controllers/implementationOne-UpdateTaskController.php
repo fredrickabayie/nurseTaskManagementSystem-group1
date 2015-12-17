64dc43d1e16ee65aeba101787ae52f2d22ab7a6c
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-12-17 21:45:41 +0000

    Created the necessary files for the implementation

---
+++ controllers/implementationOne-UpdateTaskController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fredrickabayie
+ * Date: 17/12/2015
+ * Time: 21:41
+ */
---
+++ models/UpdateTask.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: fredrickabayie
+ * Date: 17/12/2015
+ * Time: 21:44
+ */
+class UpdateTask
+{
+    
+}

6079f9357e5419d77b83248e638ad4369197ff59
davidtandoh <tandohdavid@gmail.com>
2015-12-19 23:14:58 +0000

    Added add_nurse function to the nurse class
    Tested sql statement in add_nurse class

--- models/implementation2-nurse.php
+++ models/implementation2-nurse.php
@@ -26,6 +26,21 @@ class nurse extends ntdb
         return $this->query($select_query);
     }
 
+    	/**
+	* A function to add a nurse to the database given the nurse's firstname, surname,
+    age, sex and department
+    *@param $fname firstname of nurse
+    *@param $sname surname of nurse
+    *@param $age age of nurse
+    *@param $sex sex of nurse
+    *@param $department department nurse belongs to 
+	**/
+	function add_nurse ($fname, $sname, $age, $sex, $department)
+	{
+		$insert_query = "insert into system_users set user_fname='$fname', user_sname='$sname', age='$age', sex= '$sex', department = '$department'";
+		return $this->query ($insert_query);
+	}     
+}
     
 }
 ?>
